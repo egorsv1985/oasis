b01456b5feb5ca51749275666d3d0b0991bc0a2a
egorsv1985 <egorvs2009@gmail.com>
2023-10-10 16:27:05 +0300

    fix

--- public_html/o-nas/index.php
+++ public_html/o-nas/index.php
@@ -9,19 +9,21 @@ $APPLICATION->SetTitle("О нас"); ?>
 			<div class="row">
 				<div class="col-12 col-lg-4 pt-5">
 					<div class="row">
-						<? $APPLICATION->IncludeComponent(
-							"bitrix:breadcrumb",
-							".default",
-							array(
-								"PATH" => "",	// Путь, для которого будет построена навигационная цепочка (по умолчанию, текущий путь)
-								"SITE_ID" => "s1",	// Cайт (устанавливается в случае многосайтовой версии, когда DOCUMENT_ROOT у сайтов разный)
-								"START_FROM" => "0",	// Номер пункта, начиная с которого будет построена навигационная цепочка
-							),
-							false,
-							array(
-								'HIDE_ICONS' => 'Y'
-							)
-						); ?>
+						<div class="col-3">
+							<? $APPLICATION->IncludeComponent(
+								"bitrix:breadcrumb",
+								".default",
+								array(
+									"PATH" => "",	// Путь, для которого будет построена навигационная цепочка (по умолчанию, текущий путь)
+									"SITE_ID" => "s1",	// Cайт (устанавливается в случае многосайтовой версии, когда DOCUMENT_ROOT у сайтов разный)
+									"START_FROM" => "0",	// Номер пункта, начиная с которого будет построена навигационная цепочка
+								),
+								false,
+								array(
+									'HIDE_ICONS' => 'Y'
+								)
+							); ?>
+						</div>
 						<div class="col-9">
 							<a href="#" class="about__box-content d-block">
 								<div class="fs-20 text-info">
@@ -139,22 +141,29 @@ $APPLICATION->SetTitle("О нас"); ?>
 		<div class="container position-relative">
 			<div class="row gy-5">
 				<div class="col-12 col-sm-4">
-
+					<ul class="nav nav-tabs flex-column" id="directionsTab" role="tablist">
+						<? foreach ($SECTIONS as $i => $arSection) : ?>
+							<li class="nav-item" role="presentation">
+								<button class="nav-link<?= ($i++ ? '' : ' active'); ?>" id="tab-<?= $arSection['ID']; ?>-tab" data-bs-toggle="pill" data-bs-target="#tab-<?= $arSection['ID']; ?>" type="button" role="tab" aria-controls="tab-<?= $arSection['ID']; ?>" aria-selected="true"><?= $arSection['NAME']; ?></button>
+							</li>
+						<? endforeach; ?>
+					</ul>
 					<ul class="nav nav-tabs flex-column" id="planTab" role="tablist">
 						<li class="nav-item" role="presentation">
-							<a class="nav-link" id="tab1-tab" data-toggle="tab" href="#tab1" role="tab" aria-controls="tab1" aria-selected="false">
-								Аквапарк</a>
+							<button class="nav-link" id="tab1-tab" data-bs-toggle="pill" data-bs-target="#tab-1" type="button" role="tab" aria-controls="tab-1" aria-selected="true">Аквапарк</button>
+
 						</li>
 						<li class="nav-item" role="presentation">
-							<a class="nav-link active" id="tab2-tab" data-toggle="tab" href="#tab2" role="tab" aria-controls="tab2" aria-selected="true">
-								3-ий этаж</a>
+							<button class="nav-link active" id="tab2-tab" data-bs-toggle="pill" data-bs-target="#tab-2" type="button" role="tab" aria-controls="tab-2" aria-selected="true">3-ий этаж</button>
+
 						</li>
 
 						<li class="nav-item" role="presentation">
-							<a class="nav-link" id="tab3-tab" data-toggle="tab" href="#tab3" role="tab" aria-controls="tab3" aria-selected="false">4-ий этаж</a>
+							<button class="nav-link" id="tab3-tab" data-bs-toggle="pill" data-bs-target="#tab-3" type="button" role="tab" aria-controls="tab-3" aria-selected="false">4-ий этаж</button>
+
 						</li>
 						<li class="nav-item" role="presentation">
-							<a class="nav-link" id="tab4-tab" data-toggle="tab" href="#tab4" role="tab" aria-controls="tab4" aria-selected="false">5-ий этаж</a>
+							<button class="nav-link" id="tab4-tab" data-bs-toggle="pill" data-bs-target="#tab-4" type="button" role="tab" aria-controls="tab-4" aria-selected="false">5-ий этаж</button>
 						</li>
 					</ul>
 				</div>
@@ -167,8 +176,8 @@ $APPLICATION->SetTitle("О нас"); ?>
 
 
 			<div class="tab-content my-5" id="planTabContent">
-				
-				<div class="tab-pane fade position-relative" id="tab1" role="tabpanel" aria-labelledby="tab1-tab">
+
+				<div class="tab-pane fade position-relative" id="tab-1" role="tabpanel" aria-labelledby="tab1-tab">
 					<div class="plans__img">
 						<picture>
 							<source srcset="plan2.webp" type="image/webp"><img src="plan2.png" class="w-100">
@@ -176,7 +185,7 @@ $APPLICATION->SetTitle("О нас"); ?>
 					</div>
 				</div>
 
-				<div class="tab-pane fade show active" id="tab2" role="tabpanel" aria-labelledby="tab2-tab">
+				<div class="tab-pane fade show active" id="tab-2" role="tabpanel" aria-labelledby="tab2-tab">
 					<div class="plans__img position-relative" style="padding-top: 70%; background: url(plan.svg) no-repeat center / 100% 100%;">
 						<?
 						$arSelect = array("ID", "NAME", "PROPERTY_ICO", "PROPERTY_TOP", "PROPERTY_LEFT", "PROPERTY_LINK", "PROPERTY_COLOR");
@@ -228,7 +237,7 @@ $APPLICATION->SetTitle("О нас"); ?>
 					</div>
 				</div>
 
-				<div class="tab-pane fade position-relative" id="tab3" role="tabpanel" aria-labelledby="tab3-tab">
+				<div class="tab-pane fade position-relative" id="tab-3" role="tabpanel" aria-labelledby="tab3-tab">
 					<div class="plans__img position-relative" style="
             padding-top: 70%;
             background: url(plan4.svg) no-repeat center / 100% 100%;
@@ -237,7 +246,7 @@ $APPLICATION->SetTitle("О нас"); ?>
 
 						$arSelect = array("ID", "NAME", "PROPERTY_ICO", "PROPERTY_TOP", "PROPERTY_LEFT", "PROPERTY_LINK", "PROPERTY_COLOR");
 						$arFilter = array(
-							"IBLOCK_ID" => 11,
+							"IBLOCK_ID" => 4,
 							"ACTIVE" => "Y",
 							'SECTION_ID' => 2,
 						);
@@ -258,7 +267,7 @@ center / contain;;"></div>
 
 
 					</div>
-					<div class="plans__content position-relative">
+					<!-- <div class="plans__content position-relative">
 						<div class="row">
 							<div class="col-8 col-sm-6">
 								<div class="fs-24 fw-700 text-white lh-15 mb-4">
@@ -284,9 +293,9 @@ center / contain;;"></div>
 								</div>
 							</div>
 						</div>
-					</div>
+					</div> -->
 				</div>
-				<div class="tab-pane fade position-relative" id="tab4" role="tabpanel" aria-labelledby="tab4-tab">
+				<div class="tab-pane fade position-relative" id="tab-4" role="tabpanel" aria-labelledby="tab4-tab">
 					<div class="plans__img position-relative" style="
             padding-top: 70%;
             background: url(plan5.svg) no-repeat center / 100% 100%;
@@ -295,7 +304,7 @@ center / contain;;"></div>
 
 						$arSelect = array("ID", "NAME", "PROPERTY_ICO", "PROPERTY_TOP", "PROPERTY_LEFT", "PROPERTY_LINK", "PROPERTY_COLOR");
 						$arFilter = array(
-							"IBLOCK_ID" => 11,
+							"IBLOCK_ID" => 4,
 							"ACTIVE" => "Y",
 							'SECTION_ID' => 3,
 						);
@@ -316,7 +325,7 @@ center / contain;;"></div>
 
 
 					</div>
-					<div class="plans__content position-relative">
+					<!-- <div class="plans__content position-relative">
 						<div class="row">
 							<div class="col-8 col-sm-6">
 								<div class="fs-24 fw-700 text-white lh-15 mb-4">
@@ -342,110 +351,106 @@ center / contain;;"></div>
 								</div>
 							</div>
 						</div>
-					</div>
+					</div> -->
 				</div>
 			</div>
 
 		</div>
 	</section>
-
-
-
-
 	<? $APPLICATION->IncludeComponent(
-	"bitrix:news.list", 
-	"community", 
-	array(
-		"ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"ADD_SECTIONS_CHAIN" => "N",
-		"AJAX_MODE" => "N",
-		"AJAX_OPTION_ADDITIONAL" => "",
-		"AJAX_OPTION_HISTORY" => "N",
-		"AJAX_OPTION_JUMP" => "N",
-		"AJAX_OPTION_STYLE" => "Y",
-		"CACHE_FILTER" => "N",
-		"CACHE_GROUPS" => "Y",
-		"CACHE_TIME" => "7200",
-		"CACHE_TYPE" => "A",
-		"CHECK_DATES" => "Y",
-		"DETAIL_URL" => "",
-		"DISPLAY_BOTTOM_PAGER" => "Y",
-		"DISPLAY_DATE" => "Y",
-		"DISPLAY_NAME" => "Y",
-		"DISPLAY_PICTURE" => "Y",
-		"DISPLAY_PREVIEW_TEXT" => "Y",
-		"DISPLAY_TOP_PAGER" => "N",
-		"FIELD_CODE" => array(
-			0 => "",
-			1 => "ID",
-			2 => "CODE",
-			3 => "XML_ID",
-			4 => "NAME",
-			5 => "TAGS",
-			6 => "SORT",
-			7 => "PREVIEW_TEXT",
-			8 => "PREVIEW_PICTURE",
-			9 => "DETAIL_TEXT",
-			10 => "DETAIL_PICTURE",
-			11 => "DATE_ACTIVE_FROM",
-			12 => "ACTIVE_FROM",
-			13 => "DATE_ACTIVE_TO",
-			14 => "ACTIVE_TO",
-			15 => "SHOW_COUNTER",
-			16 => "SHOW_COUNTER_START",
-			17 => "IBLOCK_TYPE_ID",
-			18 => "IBLOCK_ID",
-			19 => "IBLOCK_CODE",
-			20 => "IBLOCK_NAME",
-			21 => "IBLOCK_EXTERNAL_ID",
-			22 => "DATE_CREATE",
-			23 => "CREATED_BY",
-			24 => "CREATED_USER_NAME",
-			25 => "TIMESTAMP_X",
-			26 => "MODIFIED_BY",
-			27 => "USER_NAME",
-			28 => "PRIVEW_TEXT",
-			29 => "PRIVEW_PICTURE",
-			30 => "",
+		"bitrix:news.list",
+		"community",
+		array(
+			"ACTIVE_DATE_FORMAT" => "d.m.Y",
+			"ADD_SECTIONS_CHAIN" => "N",
+			"AJAX_MODE" => "N",
+			"AJAX_OPTION_ADDITIONAL" => "",
+			"AJAX_OPTION_HISTORY" => "N",
+			"AJAX_OPTION_JUMP" => "N",
+			"AJAX_OPTION_STYLE" => "Y",
+			"CACHE_FILTER" => "N",
+			"CACHE_GROUPS" => "Y",
+			"CACHE_TIME" => "7200",
+			"CACHE_TYPE" => "A",
+			"CHECK_DATES" => "Y",
+			"DETAIL_URL" => "",
+			"DISPLAY_BOTTOM_PAGER" => "Y",
+			"DISPLAY_DATE" => "Y",
+			"DISPLAY_NAME" => "Y",
+			"DISPLAY_PICTURE" => "Y",
+			"DISPLAY_PREVIEW_TEXT" => "Y",
+			"DISPLAY_TOP_PAGER" => "N",
+			"FIELD_CODE" => array(
+				0 => "",
+				1 => "ID",
+				2 => "CODE",
+				3 => "XML_ID",
+				4 => "NAME",
+				5 => "TAGS",
+				6 => "SORT",
+				7 => "PREVIEW_TEXT",
+				8 => "PREVIEW_PICTURE",
+				9 => "DETAIL_TEXT",
+				10 => "DETAIL_PICTURE",
+				11 => "DATE_ACTIVE_FROM",
+				12 => "ACTIVE_FROM",
+				13 => "DATE_ACTIVE_TO",
+				14 => "ACTIVE_TO",
+				15 => "SHOW_COUNTER",
+				16 => "SHOW_COUNTER_START",
+				17 => "IBLOCK_TYPE_ID",
+				18 => "IBLOCK_ID",
+				19 => "IBLOCK_CODE",
+				20 => "IBLOCK_NAME",
+				21 => "IBLOCK_EXTERNAL_ID",
+				22 => "DATE_CREATE",
+				23 => "CREATED_BY",
+				24 => "CREATED_USER_NAME",
+				25 => "TIMESTAMP_X",
+				26 => "MODIFIED_BY",
+				27 => "USER_NAME",
+				28 => "PRIVEW_TEXT",
+				29 => "PRIVEW_PICTURE",
+				30 => "",
+			),
+			"FILTER_NAME" => "",
+			"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+			"IBLOCK_ID" => "8",
+			"IBLOCK_TYPE" => "CONTENT",
+			"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+			"INCLUDE_SUBSECTIONS" => "Y",
+			"MESSAGE_404" => "",
+			"NEWS_COUNT" => "20",
+			"PAGER_BASE_LINK_ENABLE" => "N",
+			"PAGER_DESC_NUMBERING" => "N",
+			"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+			"PAGER_SHOW_ALL" => "N",
+			"PAGER_SHOW_ALWAYS" => "N",
+			"PAGER_TEMPLATE" => ".default",
+			"PAGER_TITLE" => "Новости",
+			"PARENT_SECTION" => "0",
+			"PARENT_SECTION_CODE" => "",
+			"PREVIEW_TRUNCATE_LEN" => "",
+			"PROPERTY_CODE" => array(
+				0 => "SITE",
+				1 => "ICON",
+				2 => "",
+			),
+			"SET_BROWSER_TITLE" => "N",
+			"SET_LAST_MODIFIED" => "N",
+			"SET_META_DESCRIPTION" => "N",
+			"SET_META_KEYWORDS" => "N",
+			"SET_STATUS_404" => "N",
+			"SET_TITLE" => "N",
+			"SHOW_404" => "N",
+			"SORT_BY1" => "SORT",
+			"SORT_BY2" => "SORT",
+			"SORT_ORDER1" => "DESC",
+			"SORT_ORDER2" => "ASC",
+			"STRICT_SECTION_CHECK" => "N",
+			"COMPONENT_TEMPLATE" => "community"
 		),
-		"FILTER_NAME" => "",
-		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
-		"IBLOCK_ID" => "8",
-		"IBLOCK_TYPE" => "CONTENT",
-		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-		"INCLUDE_SUBSECTIONS" => "Y",
-		"MESSAGE_404" => "",
-		"NEWS_COUNT" => "20",
-		"PAGER_BASE_LINK_ENABLE" => "N",
-		"PAGER_DESC_NUMBERING" => "N",
-		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-		"PAGER_SHOW_ALL" => "N",
-		"PAGER_SHOW_ALWAYS" => "N",
-		"PAGER_TEMPLATE" => ".default",
-		"PAGER_TITLE" => "Новости",
-		"PARENT_SECTION" => "0",
-		"PARENT_SECTION_CODE" => "",
-		"PREVIEW_TRUNCATE_LEN" => "",
-		"PROPERTY_CODE" => array(
-			0 => "SITE",
-			1 => "ICON",
-			2 => "",
-		),
-		"SET_BROWSER_TITLE" => "N",
-		"SET_LAST_MODIFIED" => "N",
-		"SET_META_DESCRIPTION" => "N",
-		"SET_META_KEYWORDS" => "N",
-		"SET_STATUS_404" => "N",
-		"SET_TITLE" => "N",
-		"SHOW_404" => "N",
-		"SORT_BY1" => "SORT",
-		"SORT_BY2" => "SORT",
-		"SORT_ORDER1" => "DESC",
-		"SORT_ORDER2" => "ASC",
-		"STRICT_SECTION_CHECK" => "N",
-		"COMPONENT_TEMPLATE" => "community"
-	),
-	false
-); ?>
+		false
+	); ?>
 </main>
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
