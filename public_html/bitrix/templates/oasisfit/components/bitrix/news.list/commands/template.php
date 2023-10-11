7e0b15c2b8b4a95ec3df1c96686ed7a9e81d75fb
egorsv1985 <egorvs2009@gmail.com>
2023-10-11 16:57:11 +0300

    fix commands start modal team

--- public_html/bitrix/templates/oasisfit/components/bitrix/news.list/commands/template.php
+++ public_html/bitrix/templates/oasisfit/components/bitrix/news.list/commands/template.php
@@ -24,9 +24,8 @@ if (empty($arResult["ITEMS"])) return false;
 			$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
 			$slideCount++; // Увеличиваем счетчик слайдов
 		?>
-
 			<div class="swiper-slide">
-				<a href="#team" class="d-block text-center position-relative" data-popup="#team">
+				<button type="button" class="d-block text-center position-relative" data-bs-toggle="modal" data-bs-target="#team">
 					<div class="swiper__box-img">
 						<picture>
 							<source srcset="<?= $arItem["PREVIEW_PICTURE"]["SRC"] ?>" type="image/webp">
@@ -39,27 +38,21 @@ if (empty($arResult["ITEMS"])) return false;
 							<div class="fs-16 lh-12 text-primary"><?= $arItem['PROPERTIES']['POST']['VALUE']; ?></div>
 						</div>
 					</div>
-				</a>
+				</button>
 			</div>
-
 		<? endforeach; ?>
 	</div>
 </div>
 
-</div>
-</div>
 
-<div class="row mb-3">
-	<div class="col-12 col-md-3">
-		<h2 class="fs-40 fw-700 lh-12 mb-4"><? $APPLICATION->ShowTitle(true); ?></h2>
-	</div>
-</div>
+<? $this->SetViewTarget('swiper__control'); ?>
 <div class="swiper__control d-flex align-items-center gap-4">
 	<span class="swiper__span fs-20 fw-700 lh-12">01</span>
 	<div class="swiper-pagination position-relative d-flex align-items-center"></div>
-	<span class="swiper__span fs-20 fw-700 lh-12">05</span>
+	<span class="swiper__span fs-20 fw-700 lh-12"><? echo ($slideCount < 10) ? str_pad($slideCount, 2, '0', STR_PAD_LEFT) : $slideCount; ?></span>
 	<div class="swiper__buttons d-flex gap-1">
 		<div class="swiper-button-prev rounded-circle border-success border"></div>
 		<div class="swiper-button-next rounded-circle border-success border"></div>
 	</div>
 </div>
+<? $this->EndViewTarget(); ?>
--- public_html/bitrix/templates/oasisfit/footer.php
+++ public_html/bitrix/templates/oasisfit/footer.php
@@ -39,48 +39,14 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 </footer>
 
 </div>
-
-<div id="callback" aria-hidden="true" class="popup">
-	<div class="popup__wrapper">
-		<div class="popup__content bg-secondary rounded-3 d-flex justify-content-center align-items-center position-relative">
-			<button data-close type="button" class="popup__close">
-				<svg width="45" height="46" viewBox="0 0 45 46" fill="none" xmlns="http://www.w3.org/2000/svg">
-					<path fill-rule="evenodd" clip-rule="evenodd" d="M22.4999 24.6545L32.7543 34.9117L34.7455 32.9232L24.4883 22.666L34.7455 12.4117L32.7571 10.4204L22.4999 20.6776L12.2455 10.4204L10.2571 12.4117L20.5115 22.666L10.2571 32.9204L12.2455 34.9117L22.4999 24.6545Z" fill="#262722" />
-				</svg>
-			</button>
-
-			<form action="#" class="popup__form form border-3 d-flex flex-column justify-content-center ">
-				<div class="fs-24 text-center fw-700 mb-2">Оставить заявку</div>
-				<p class="fs-16 text-info text-center lh-13 mb-4">Оставьте свои контакты и мы с вами свяжемся</p>
-				<div class="form-group position-relative mb-3">
-					<input type="text" class="form-control form__input bg-light rounded-2 text-info p-3 h-100" name="name" id="name" placeholder="" required />
-					<label for="name" class="form-label form__label fs-16 text-info position-absolute top-50 mb-0">Имя*</label>
-				</div>
-				<div class="form-group position-relative mb-4">
-					<input type="text" class="form-control form__input bg-light rounded-2 text-info p-3 h-100" name="tel" id="tel" placeholder="" required />
-					<label for="tel" class="form-label form__label fs-16 text-info position-absolute top-50 mb-0 ">Телефон*</label>
-				</div>
-				<button type="submit" class="form__btn btn fs-16 fw-600 px-3 py-3 btn-primary w-100 border-0 mb-4">
-					Отправить
-				</button>
-				<p class="fs-14 lh-12 text-info text-center">
-					Нажимая на кнопку, вы даете согласие на обработку персональных
-					данных
-				</p>
-
-			</form>
-		</div>
-	</div>
-</div>
-<div id="team" aria-hidden="true" class="popup popup-team bg-secondary">
-	<div class="popup__wrapper">
-		<div class="popup__content border border-success rounded-3 position-relative">
-			<button data-close type="button" class="popup__close rounded-circle">
-				<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 50 50" fill="none">
-					<line x1="14.747" y1="34.546" x2="34.546" y2="14.747" stroke="white" />
-					<line x1="15.4541" y1="14.7469" x2="35.2531" y2="34.5459" stroke="white" />
-				</svg>
-			</button>
+<!-- Modal -->
+<div class="modal fade" id="team" tabindex="-1" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">				</button>
+			</div>
+			<div class="modal-body">
 			<div class="row">
 				<div class="col-12 col-lg-6 position-relative">
 					<div class="popup__box-fon text-center position-relative h-100">
@@ -128,9 +94,44 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 					</div>
 				</div>
 			</div>
+
+			</div>
 		</div>
 	</div>
 </div>
+<div id="callback" aria-hidden="true" class="popup">
+	<div class="popup__wrapper">
+		<div class="popup__content bg-secondary rounded-3 d-flex justify-content-center align-items-center position-relative">
+			<button data-close type="button" class="popup__close">
+				<svg width="45" height="46" viewBox="0 0 45 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path fill-rule="evenodd" clip-rule="evenodd" d="M22.4999 24.6545L32.7543 34.9117L34.7455 32.9232L24.4883 22.666L34.7455 12.4117L32.7571 10.4204L22.4999 20.6776L12.2455 10.4204L10.2571 12.4117L20.5115 22.666L10.2571 32.9204L12.2455 34.9117L22.4999 24.6545Z" fill="#262722" />
+				</svg>
+			</button>
+
+			<form action="#" class="popup__form form border-3 d-flex flex-column justify-content-center ">
+				<div class="fs-24 text-center fw-700 mb-2">Оставить заявку</div>
+				<p class="fs-16 text-info text-center lh-13 mb-4">Оставьте свои контакты и мы с вами свяжемся</p>
+				<div class="form-group position-relative mb-3">
+					<input type="text" class="form-control form__input bg-light rounded-2 text-info p-3 h-100" name="name" id="name" placeholder="" required />
+					<label for="name" class="form-label form__label fs-16 text-info position-absolute top-50 mb-0">Имя*</label>
+				</div>
+				<div class="form-group position-relative mb-4">
+					<input type="text" class="form-control form__input bg-light rounded-2 text-info p-3 h-100" name="tel" id="tel" placeholder="" required />
+					<label for="tel" class="form-label form__label fs-16 text-info position-absolute top-50 mb-0 ">Телефон*</label>
+				</div>
+				<button type="submit" class="form__btn btn fs-16 fw-600 px-3 py-3 btn-primary w-100 border-0 mb-4">
+					Отправить
+				</button>
+				<p class="fs-14 lh-12 text-info text-center">
+					Нажимая на кнопку, вы даете согласие на обработку персональных
+					данных
+				</p>
+
+			</form>
+		</div>
+	</div>
+</div>
+
 
 
 </body>
